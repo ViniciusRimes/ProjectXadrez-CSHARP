352645e13e0f515e32cca521014f14fcbf95ed76
ViniciusRimes <viniciusrimess@gmail.com>
2023-12-11 20:14:46 -0300

    Create class Position

---
+++ Board/Entities/Position.cs
@@ -0,0 +1,19 @@
+namespace Board
+{
+    public class Position
+    {
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+        public Position(){}
+        public Position(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+        public override string ToString()
+        {
+            return $"{Line}, {Column}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Board;
+
+namespace ProjectXadrez
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            
+        }
+    }
+}
